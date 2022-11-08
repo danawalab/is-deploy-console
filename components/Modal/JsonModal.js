2dba8ac48559b47527fc84f115f4a687d8f875dd
Dokkabei97 <wkdrn970@naver.com>
2022-11-09 08:52:41 +0900

    docs: 문서 주석 추가

--- components/Modal/JsonModal.js
+++ components/Modal/JsonModal.js
@@ -14,10 +14,16 @@ export default function JsonModal({open, onClose, config, data, id}) {
         setJson(data);
     }, [data])
 
+    /**
+     * 수정한 json을 저장한다.
+     * 만약 수정 버튼이 활성화돼있다면 alert를 통해 수정 버튼을 비활성화 유도한다.
+     * @returns {Promise<void>}
+     */
     const save = async () => {
         if (isOpen(textAreaOpen)) {
             alert("수정을 비활성화해주세요.");
         } else {
+            // axios는 값을 object 형태로 감싸서 서버로 보내기에 content-type을 text로 지정해 그대로 넘기게 변경
             await axios.put(`http://localhost:3000/api/${id}`, json, {
                 "headers": {
                     "content-type": "application/text",
@@ -26,12 +32,43 @@ export default function JsonModal({open, onClose, config, data, id}) {
         }
     }
 
+    /**
+     * console에 수정된 json을 agent에 동기화해준다.
+     * @returns {Promise<void>}
+     */
     const sync = async () => {
         if (isOpen(textAreaOpen)) {
             alert("수정을 비활성화해주세요.");
         } else {
             console.log("sync");
         }
+    }
+
+    /**
+     * init.json을 만들어 준다.
+     * 첫 화면에서 json을 수정해 새 서비스를 정의했을 때 init.json을 보는데
+     * init.json이 없으면 오류가 나기에 없어졌을 경우를 위한 기능
+     * @returns {Promise<void>}
+     */
+    const createInitJson = async () => {
+        let init = {
+            "service": "",
+            "consoleInfo": {
+                "address": ""
+            },
+            "agentInfo": {
+                "name": "",
+                "ip": "",
+                "port": ""
+            },
+            "node": []
+        }
+
+        await axios.post("http://localhost:3000/api/${id}", init, {
+            "headers": {
+                "content-type": "application/text",
+            },
+        });
     }
 
     const isOpen = (open) => {
@@ -92,7 +129,16 @@ export default function JsonModal({open, onClose, config, data, id}) {
                             >
                                 동기화
                             </Button>
-                        ) : <></>}
+                        ) : (
+                            <Button
+                                variant={"contained"}
+                                color={"success"}
+                                onClick={createInitJson}
+                                className={styles.mL}
+                            >
+                                init.json 생성
+                            </Button>
+                        )}
                     </Box>
                 </Box>
             </Modal>
--- pages/api/[service].js
+++ pages/api/[service].js
@@ -1,33 +1,77 @@
 import fs from 'fs';
 
 export default function handler(req, res) {
-    let path
-    if (req.method === 'GET') {
-        if (req.query['service'] === 'config') {
-            path = "./config/config.json"
-        } else {
-            path = `./config/service/${req.query['service']}/${req.query['service']}.json`
-        }
+    let path;
 
+    /**
+     * json을 읽어 반환한다.
+     */
+    if (isMethodGet(req)) {
+        /**
+         * req.query['service'] 가 config면 './config/config.json'
+         * 아닐 경우 동적으로 경로가 지정된다 `./config/service/${req.query['service']}/${req.query['service']}.json`
+         * @type {string|string}
+         */
+        path = isQueryConfig(req) ?
+            './config/config.json' :
+            `./config/service/${req.query['service']}/${req.query['service']}.json`;
+
+        fileRead(path);
+    }
+
+    /**
+     * 수정된 json 값을 받아 json 파일을 덮어 쓴다.
+     */
+    if (isMethodPut(req)) {
+        let json = JSON.stringify(JSON.parse(req.body));
+
+        path = isQueryConfig(req) ?
+            './config/config.json' :
+            `./config/service/${req.query['service']}/${req.query['service']}.json`;
+
+        fs.writeFileSync(path, json, 'utf-8');
+    }
+
+    /**
+     * init.json을 만든다.
+     */
+    if (isMethodPost(req)) {
+        let json = JSON.stringify(JSON.parse(req.body));
+        path = './config/service/init.json';
+
+        fs.writeFileSync(path, json, 'utf-8');
+    }
+
+    /**
+     * 지정된 path에 있는 json을 읽어 반환한다.
+     * 만약 path가 없으면 path는 './config/service/init.json' 지정하고 재귀함 수로 한 번 더 호출한다.
+     * @param path
+     */
+    function fileRead(path) {
         fs.readFile(path, 'utf-8', (err, data) => {
             if (err !== null) {
-                console.error(err);
+                path = './config/service/init.json';
+                fileRead(path);
             } else {
                 return res.status(200).json(data);
             }
         });
     }
 
-    if (req.method === 'PUT') {
-        let json = JSON.stringify(JSON.parse(req.body));
+    function isMethodGet(req) {
+        return req.method === 'GET';
+    }
 
-        if (req.query['service'] === 'config') {
-            path = "./config/config.json"
-        } else {
-            path = `./config/service/${req.query['service']}/${req.query['service']}.json`
-        }
+    function isMethodPut(req) {
+        return req.method === 'PUT';
+    }
 
-        fs.writeFileSync(path, json, 'utf-8');
+    function isMethodPost(req) {
+        return req.method === 'POST';
+    }
+
+    function isQueryConfig(req) {
+        return req.query['service'] === 'config';
     }
 }
 
